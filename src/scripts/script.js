abbfbf87c81a833418565bca3d7487adf2ed6341
TalkativeDiv <85126640+TalkativeDiv@users.noreply.github.com>
2022-05-24 14:16:45 -0400

    Added Label

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -16,8 +16,10 @@ const menuOpen = document.querySelector(".menu");
 const menuClose = document.querySelector(".close");
 const overlay = document.querySelector(".overlay");
 const btnNext = document.getElementById("btnNext");
+const lblName = document.getElementById('lblName')
 const img = document.getElementById("book");
 const pages = ['/public/pages/Abhirath.jpg','/public/pages/Shakuntala.jpg','/public/pages/Arun.jpg','/public/pages/cover.png'];
+const names = ['Abhi','Shakuntala','Arun','Cover'];
 let index = 0;
 
 menuOpen.addEventListener("click", () => {
@@ -33,4 +35,5 @@ btnNext.addEventListener('click', () => {
   if (index === 3) index = 0;
   else index++;
   img.src = pages[index];
+	lblName.innerText = names[index];
 });
